5d6638075fc50fa6c4aa0689a6e84d8031796541
glogowsk <glogowsk@glogowsk-mbp.marketo.net>
2019-03-25 12:52:04 -0700

    added create all script

---
+++ create_all.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#./01-firewall.sh
+./10-ambari.sh
+./12-metrics.sh
+./20-namenodes.sh
+./22-resourcemanagers.sh
+./24-hbasemasters.sh
+./30-zookeepers.sh
+./40-kafkabrokers.sh
+./80-datanodes.sh
+./99-cleanup.sh
+
