6ef4c4a24b8b41f2dee9a7e2bee30727ecd734b1
glogowsk <glogowsk@glogowsk-mbp.marketo.net>
2019-03-25 14:46:13 -0700

    postgres server changes

--- scripts/stubs/postgresql-server.sh
+++ scripts/stubs/postgresql-server.sh
@@ -1,18 +1,23 @@
 # postgresql-server.sh
 
+/usr/pgsql-9.6//bin/postgresql96-setup initdb
+
 cat > /var/lib/pgsql/9.6/data/pg_hba.conf <<EOF
 local   all   postgres                                                           peer
-host    all   postgres                                   127.0.0.1/32            ident
-host    all   postgres                                   ::1/128                 ident
-local   all   ambari,mapred,hive,ranger,rangerkms                                md5
-host    all   ambari,mapred,hive,ranger,rangerkms        0.0.0.0/0               md5
-host    all   ambari,mapred,hive,ranger,rangerkms        ::/0                    md5
+host    all   postgres                                         127.0.0.1/32      ident
+host    all   postgres                                         ::1/128           ident
+local   all   ambari,mapred,oozie,hive,ranger,rangerkms                          md5
+host    all   ambari,mapred,oozie,hive,ranger,rangerkms        0.0.0.0/0         md5
+host    all   ambari,mapred,oozie,hive,ranger,rangerkms        ::/0              md5
 EOF
+
+sed -i -e"s/#listen_addresses = 'localhost'/listen_addresses = '*'/" /var/lib/pgsql/9.6/data/postgresql.conf
+sed -i -e"s/#port = 5432/port = 5432/" /var/lib/pgsql/9.6/data/postgresql.conf
 
 /bin/systemctl enable postgresql-9.6
 /bin/systemctl start postgresql-9.6.service
 
-for app in ambari mapred hive ranger rangerkms
+for app in ambari mapred oozie hive ranger rangerkms
 do
 
   sudo -u postgres createuser $app
