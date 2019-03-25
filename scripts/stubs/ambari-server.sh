1bdd606ee4e73070b2ed73cc94d37831edb90dd4
glogowsk <glogowsk@glogowsk-mbp.marketo.net>
2019-03-25 16:40:45 -0700

    Added Ambari schema

--- scripts/stubs/ambari-server.sh
+++ scripts/stubs/ambari-server.sh
@@ -1,6 +1,15 @@
 # ambari-server.sh
 
 yum install -y ambari-server
-/sbin/ambari-server setup --silent --jdbc-db=postgres --jdbc-driver=/opt/postgresql/postgresql-42.2.5.jar
+/sbin/ambari-server setup \
+	--silent \
+	--jdbc-db=postgres \
+	--jdbc-driver=/opt/postgresql/postgresql-42.2.5.jar \
+	--database=postgres \
+	--databasehost=postgres \
+	--databaseport=5432 \
+	--databasename=ambari \
+	--databaseusername=ambari \
+	--databasepassword=++++++++++++++
 /sbin/ambari-server start
 
