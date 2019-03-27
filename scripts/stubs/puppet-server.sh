b7d125c3f3f0c60dba328042a91d9d8cce699da4
glogowsk <glogowsk@glogowsk-mbp.marketo.net>
2019-03-26 17:13:40 -0700

    changed foreman install order

--- scripts/stubs/puppet-server.sh
+++ scripts/stubs/puppet-server.sh
@@ -16,6 +16,23 @@ yum install -y puppetserver
 /opt/puppetlabs/bin/puppet module install puppetlabs-sshkeys_core --version 1.0.2
 /opt/puppetlabs/bin/puppet module install puppetlabs-host_core --version 1.0.2
 
+
+yum -y install https://yum.theforeman.org/releases/1.21/el7/x86_64/foreman-release.rpm
+yum -y install foreman-installer
+foreman-installer
+
+
+{ cat | sudo -u postgres psql; } << EOF
+CREATE DATABASE puppetdb;
+CREATE USER puppetdb WITH PASSWORD '++++++++++++++';
+GRANT ALL PRIVILEGES ON DATABASE puppetdb TO puppetdb;
+\connect puppetdb;
+CREATE SCHEMA puppetdb AUTHORIZATION puppetdb;
+ALTER SCHEMA puppetdb OWNER TO puppetdb;
+ALTER ROLE puppetdb SET search_path to 'puppetdb', 'public';
+EOF
+
+
 yum install -y puppetdb
 /opt/puppetlabs/bin/puppetdb ssl-setup -f
 
@@ -33,6 +50,4 @@ EOF
 /bin/systemctl restart puppetserver.service
 /opt/puppetlabs/bin/puppet agent --test
 
-yum -y install https://yum.theforeman.org/releases/1.21/el7/x86_64/foreman-release.rpm
-yum -y install foreman-installer
 
