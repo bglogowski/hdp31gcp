6cab138ae0b65618ac621964b327787c6fbdc1ce
glogowsk <glogowsk@glogowsk-mbp.local>
2019-03-27 10:01:04 -0700

    puppetdb requires postgres 9.6

--- scripts/stubs/puppet-server.sh
+++ scripts/stubs/puppet-server.sh
@@ -22,7 +22,31 @@ yum -y install foreman-installer
 foreman-installer
 
 
-{ cat | sudo -u postgres psql; } << EOF
+yum install -y https://download.postgresql.org/pub/repos/yum/9.6/redhat/rhel-7-x86_64/pgdg-centos96-9.6-3.noarch.rpm
+yum install -y postgresql96 postgresql96-libs postgresql96-server
+
+/usr/pgsql-9.6/bin/postgresql96-setup initdb
+
+cat > /var/lib/pgsql/9.6/data/pg_hba.conf <<EOF
+# TYPE  DATABASE        USER            ADDRESS                 METHOD
+local   all             postgres                                peer
+host    all             postgres        127.0.0.1/32            ident
+host    all             postgres        ::1/128                 ident
+host    all             puppetdb        127.0.0.1/32            md5
+host    all             puppetdb        ::1/128                 md5
+host    all             puppetdb        10.0.0.0/8              md5
+EOF
+
+sed -i -e"s/#listen_addresses = 'localhost'/listen_addresses = '*'/" /var/lib/pgsql/9.6/data/postgresql.conf
+sed -i -e"s/#port = 5432/port = 5436/" /var/lib/pgsql/9.6/data/postgresql.conf
+
+/bin/systemctl enable postgresql-9.6
+/bin/systemctl start postgresql-9.6.service
+
+
+
+
+{ cat | sudo -u postgres psql -p 5436; } << EOF
 CREATE DATABASE puppetdb;
 CREATE USER puppetdb WITH PASSWORD '++++++++++++++';
 GRANT ALL PRIVILEGES ON DATABASE puppetdb TO puppetdb;
@@ -38,7 +62,7 @@ yum install -y puppetdb
 
 cat > /etc/puppetlabs/puppetdb/conf.d/database.ini <<EOF
 [database]
-subname = //127.0.0.1:5432/puppetdb
+subname = //127.0.0.1:5436/puppetdb
 username = puppetdb
 password = ++++++++++++++
 gc-interval = 60
